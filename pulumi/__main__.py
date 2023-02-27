908a0a3810373e78a7ec1d14cf3e053b3f719098
brian schaefer <brian.schaefer@bonfyreapp.com>
2023-02-26 19:11:14 -0600

    WIP. The loadbalancer / ingress configuration is not working.

--- pulumi/__main__.py
+++ pulumi/__main__.py
@@ -1,7 +1,9 @@
+import base64
 import pulumi
 import pulumi_eks as eks
 import pulumi_aws as aws
 import pulumi_kubernetes as k8s
+import pulumi_docker as docker
 
 cluster_name = "zephyr-cluster"
 cluster_tag = f"kubernetes.io/cluster/{cluster_name}"
@@ -75,3 +77,196 @@ cluster = eks.Cluster(
 
 # Export the cluster's kubeconfig.
 pulumi.export("kubeconfig", cluster.kubeconfig)
+
+# The rest of this is the loadbalancer stuff
+
+import json
+
+aws_lb_ns = "aws-lb-controller"
+service_account_name = f"system:serviceaccount:{aws_lb_ns}:aws-lb-controller-serviceaccount"
+oidc_arn = cluster.core.oidc_provider.arn
+oidc_url = cluster.core.oidc_provider.url
+
+# Create IAM role for AWS LB controller service account
+iam_role = aws.iam.Role(
+    "aws-loadbalancer-controller-role",
+    assume_role_policy=pulumi.Output.all(oidc_arn, oidc_url).apply(
+        lambda args: json.dumps(
+            {
+                "Version": "2012-10-17",
+                "Statement": [
+                    {
+                        "Effect": "Allow",
+                        "Principal": {
+                            "Federated": args[0],
+                        },
+                        "Action": "sts:AssumeRoleWithWebIdentity",
+                        "Condition": {
+                            "StringEquals": {f"{args[1]}:sub": service_account_name},
+                        },
+                    }
+                ],
+            }
+        )
+    ),
+)
+
+with open("files/iam_policy.json") as policy_file:
+    policy_doc = policy_file.read()
+
+iam_policy = aws.iam.Policy(
+    "aws-loadbalancer-controller-policy",
+    policy=policy_doc,
+    opts=pulumi.ResourceOptions(parent=iam_role),
+)
+
+# Attach IAM Policy to IAM Role
+aws.iam.PolicyAttachment(
+    "aws-loadbalancer-controller-attachment",
+    policy_arn=iam_policy.arn,
+    roles=[iam_role.name],
+    opts=pulumi.ResourceOptions(parent=iam_role),
+)
+
+provider = k8s.Provider("provider", kubeconfig=cluster.kubeconfig)
+
+namespace = k8s.core.v1.Namespace(
+    f"{aws_lb_ns}-ns",
+    metadata={
+        "name": aws_lb_ns,
+        "labels": {
+            "app.kubernetes.io/name": "aws-load-balancer-controller",
+        }
+    },
+    opts=pulumi.ResourceOptions(
+        provider=provider,
+        parent=provider,
+    )
+)
+
+service_account = k8s.core.v1.ServiceAccount(
+    "aws-lb-controller-sa",
+    metadata={
+        "name": "aws-lb-controller-serviceaccount",
+        "namespace": namespace.metadata["name"],
+        "annotations": {
+            "eks.amazonaws.com/role-arn": iam_role.arn.apply(lambda arn: arn)
+        }
+    }
+)
+
+# This transformation is needed to remove the status field from the CRD
+# otherwise the Chart fails to deploy
+def remove_status(obj, opts):
+    if obj["kind"] == "CustomResourceDefinition":
+        del obj["status"]
+
+k8s.helm.v3.Chart(
+    "lb", k8s.helm.v3.ChartOpts(
+        chart="aws-load-balancer-controller",
+        version="1.2.0",
+        fetch_opts=k8s.helm.v3.FetchOpts(
+            repo="https://aws.github.io/eks-charts"
+        ),
+        namespace=namespace.metadata["name"],
+        values={
+            "region": "us-east-1",
+            "serviceAccount": {
+                "name": "aws-lb-controller-serviceaccount",
+                "create": False,
+            },
+            "vpcId": cluster.eks_cluster.vpc_config.vpc_id,
+            "clusterName": cluster.eks_cluster.name,
+            "podLabels": {
+                "stack": pulumi.get_stack(),
+                "app": "aws-lb-controller"
+            }
+        },
+        transformations=[remove_status]
+    ), pulumi.ResourceOptions(
+        provider=provider, parent=namespace
+    )
+)
+
+# Third section - setting up the ECR
+ecr_repo = aws.ecr.Repository("zephyr-ecr-repo")
+
+repo_policy = aws.ecr.RepositoryPolicy(
+    "zephyr-ecr-repo-policy",
+    policy="""{
+        "Version": "2008-10-17",
+        "Statement": [
+            {
+                "Sid": "new policy",
+                "Effect": "Allow",
+                "Principal": "*",
+                "Action": [
+                    "ecr:GetDownloadUrlForLayer",
+                    "ecr:BatchGetImage",
+                    "ecr:BatchCheckLayerAvailability",
+                    "ecr:PutImage",
+                    "ecr:InitiateLayerUpload",
+                    "ecr:UploadLayerPart",
+                    "ecr:CompleteLayerUpload",
+                    "ecr:DescribeRepositories",
+                    "ecr:GetRepositoryPolicy",
+                    "ecr:ListImages",
+                    "ecr:DeleteRepository",
+                    "ecr:BatchDeleteImage",
+                    "ecr:SetRepositoryPolicy",
+                    "ecr:DeleteRepositoryPolicy"
+                ]
+            }
+        ]
+    }""",
+    repository=ecr_repo.name
+)
+
+ecr_access_policy = aws.iam.Policy(
+    "ecr-access-iam-policy",
+    policy="""{
+      "Version": "2012-10-17",
+      "Statement": [
+        {
+          "Effect": "Allow",
+          "Action": [
+            "ecr:BatchCheckLayerAvailability",
+            "ecr:BatchGetImage",
+            "ecr:GetDownloadUrlForLayer",
+            "ecr:GetAuthorizationToken"
+          ],
+          "Resource": "*"
+        }
+      ]
+    }"""
+)
+
+nodeInstanceRole = aws.iam.RolePolicyAttachment(
+    "eks-NodeInstanceRole-policy-attach",
+    role=cluster.instance_roles[0].name,
+    policy_arn=ecr_access_policy.arn,
+)
+
+pulumi.export("ecr_repo_url", ecr_repo.repository_url)
+
+# Get registry info (creds and endpoint).
+def getRegistryInfo(rid):
+    creds = aws.ecr.get_credentials(registry_id=rid)
+    decoded = base64.b64decode(creds.authorization_token).decode()
+    parts = decoded.split(':')
+    if len(parts) != 2:
+        raise Exception("Invalid credentials")
+    return docker.ImageRegistry(creds.proxy_endpoint, parts[0], parts[1])
+image_name = ecr_repo.repository_url
+registry_info = ecr_repo.registry_id.apply(getRegistryInfo)
+
+image = docker.Image('zephyr-image',
+    build='../',
+    image_name=image_name,
+    registry=registry_info,
+)
+
+# Export the base and specific version image name.
+pulumi.export('baseImageName', image.base_image_name)
+pulumi.export('fullImageName', image.image_name)
+
